3941e09fb7e673d823dd835685e1fab75941f82b
AndrianaTodorova <rosiani@mail.bg>
2018-02-05 13:43:23 +0200

    Better-Solution

--- 16.ListsExercices/04.LongestIncreasingSubsequence/Program.cs
+++ 16.ListsExercices/04.LongestIncreasingSubsequence/Program.cs
@@ -10,6 +10,49 @@ namespace _04.LongestIncreasingSubsequence
     {
         static void Main(string[] args)
         {
+			 var sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var longestSeq = FindLongestIncreasingSubsequence(sequence);
+            Console.WriteLine(string.Join(" ", longestSeq));
+        }
+		 public static int[] FindLongestIncreasingSubsequence(int[] sequence)
+        {
+            int maxLength = 0;
+            int lastIndex = -1;
+            int[] length = new int[sequence.Length];
+            int[] prev = new int[sequence.Length];
+
+            for (int index = 0; index < sequence.Length; index++)
+            {
+                length[index] = 1;
+                prev[index] = -1;
+
+                for (int i = 0; i < index; i++)
+                {
+                    if (sequence[i] < sequence[index] && length[i] + 1 > length[index])
+                    {
+                        length[index] = length[i] + 1;
+                        prev[index] = i;
+                    }
+                }
+
+                if (length[index] > maxLength)
+                {
+                    maxLength = length[index];
+                    lastIndex = index;
+                }
+            }
+
+            var longestMatrix = new int[maxLength];
+            int currentIndex = maxLength - 1;
+
+            while (lastIndex != -1)
+            {
+                longestMatrix[currentIndex] = sequence[lastIndex];
+                currentIndex--;
+                lastIndex = prev[lastIndex];
+            }
+
+            return longestMatrix.ToArray();
         }
     }
 }
